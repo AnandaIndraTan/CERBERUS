d551dfae67483013888275f4f68800957b1f465c
Ananda Indra Tan <anandaindratan45@gmail.com>
2025-02-04 14:46:58 +0800

    revert to original

--- head.py
+++ head.py
@@ -23,30 +23,23 @@ class Head:
         
 
     def head_setup(self):
-        config_instructions = [
-            f"{'do not ' if not v else ''}{' '.join(k.split('_'))}" if isinstance(v, bool) else
-            f"{' '.join(k.split('_'))} is {v} miliseconds" if isinstance(v, (int, float)) and 'time' in k else
+        instruct_list = ", ".join([
+            f"don't {' '.join(k.split('_'))}" if isinstance(v, bool) and not v else
+            f"{' '.join(k.split('_'))}" if isinstance(v, bool) and v else
+            f"{' '.join(k.split('_'))} is {v}" if isinstance(v, (int, float)) else
             f"{' '.join(k.split('_'))} is {v}"
             for k, v in self.pen_test_config.items()
-        ]
+        ])
         
-        numbered_instructions = [
-            f"{i}. {instruction}"
-            for i, instruction in enumerate(config_instructions, start=4)
-        ]
-        
-        instruct_list = "\n".join(numbered_instructions)
-        
-        return f"""System Message: You are an expert at penetration testing. 
-You are assigned with a tool, {self.tool}, to perform a security scan on a target system.
-Here are important things to note:
-1. always return a valid string command
-2. do not create another file
-3. format your response as valid JSON with 'command' and 'output' fields
-4. ensure the command field contains the exact command to run
-{instruct_list}
-
-Task: {self.prompt}"""
+        return f"""You are an expert at using {self.tool} for penetration testing.
+        {instruct_list}.
+        Time is in miliseconds.
+        Return ONLY a JSON object in this format:
+        {{
+            "command": "<the command to execute>",
+            "output": "<the output from the command>"
+        }}
+        Task: {self.prompt}"""
 
     def head(self):
         llm = ChatOpenAI(
@@ -72,6 +65,7 @@ Task: {self.prompt}"""
                 try:
                     parsed_response = json.loads(cleaned_result)
                     return parsed_response
+
                 except json.JSONDecodeError:
                     pass
             
@@ -87,6 +81,7 @@ Task: {self.prompt}"""
                         json_str = result[json_start:json_end]
                         parsed_response = json.loads(json_str)
                         return parsed_response
+                        
                 except json.JSONDecodeError:
                     pass
                 
